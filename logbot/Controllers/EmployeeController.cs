4c7840be8b710234b9c932035d736713d4422472
vJhonatan <jhonatan99653@gmail.com>
2025-03-31 19:57:40 -0300

    feat: add employee service and controller

---
+++ logbot/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using logbot.Models;
+using logbot.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace logbot.Controllers
+{
+    [Controller]
+    [Route("api/employees")]
+    public class EmployeeController : ControllerBase
+    {   
+        private readonly EmployeeService _employeeService;
+        
+        public EmployeeController(EmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpPost]
+        public ActionResult newEmployee([FromBody] EmployeeModel employee)
+        {
+            _employeeService.newEmployee(employee);
+            return Ok(new { message = "Funcionário adicionado!"});
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            var employees = _employeeService.GetAll();
+            return Ok(employees);
+        }
+
+    }
+}
---
+++ logbot/Services/EmployeeService.cs
@@ -0,0 +1,26 @@
+﻿    using logbot.Database;
+    using logbot.Models;
+
+    namespace logbot.Services
+    {
+        public class EmployeeService
+        {
+            private readonly AppDbContext _context;
+
+            public EmployeeService(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public void newEmployee(EmployeeModel employee)
+            {
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
+            }
+
+            public IEnumerable<EmployeeModel> GetAll()
+            {
+                return _context.Employees.ToList();
+            }
+        }
+    }
